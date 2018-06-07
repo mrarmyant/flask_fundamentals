b88857e88c391864e16afad6cf16776436c2f8a3
Taylor Fry <taylor.fry@fryco.com>
2018-06-07 20:14:39 +0000

    added understand routing, has a wonky loop

---
+++ understandrouting.py
@@ -0,0 +1,23 @@
+from flask import Flask  # Import Flask to allow us to create our app.
+app = Flask(__name__)    # Global variable __name__ tells Flask whether or not we are running the file
+
+@app.route("/")
+def helloworld():
+    return "Hello World"
+
+@app.route("/dojo")
+def dojo():
+    return "Dojo!"
+
+@app.route("/say/<name>")
+def say(name):
+    return "Hi", name + "!"
+
+@app.route("/repeat/<num>/<name>")
+def hello(num,name):
+    output=""
+    for i in (0,30,1):
+        output+= name + " <br>"
+    return output
+
+app.run(debug=True, host="0.0.0.0",port=8080)
