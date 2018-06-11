de0255b19bba49bf5864387c3eb9d156dcea51d4
Taylor Fry <taylor.fry@fryco.com>
2018-06-11 16:28:31 +0000

    Added a fruit app using post an date time items

--- dojosurvey/assignment/server.py
+++ dojosurvey/assignment/server.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template  # Import Flask to allow us to create our app.
 app = Flask(__name__)    # Global variable __name__ tells Flask whether or not we are running the file
 
-@app.route('/play')
 
-def hello_world():
+
+@app.route("/")
+def index():
     return render_template("index.html")
 
-@app.route("/play/<boxnum>/<color>")
-def makeBoxes(boxnum,color):
-    return render_template("index.html", boxnum=int(boxnum), color=color)
+@app.route("/result", methods=['POST'])
+def result():
+    print (request.form['name'])
+    print (request.form['loc'])
+
+    return render_template("results.html", name=request.form['name'], loc=request.form['loc'])
 app.run(debug=True, host="0.0.0.0",port=8080)
